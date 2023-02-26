c529d3b33f726a17ee6fd1805788f59f0e6bc6a1
BiancaIoana03 <100294143+biancaioana3@users.noreply.github.com>
2023-02-26 22:15:52 +0200

    homework/ex_1

--- lab_1/src/Compulsory.java
+++ lab_1/src/Compulsory.java
@@ -1,13 +1,13 @@
-class HelloWorld {
+public class Compulsory {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); 
+        System.out.println("Hello World!");
         String[] languages = new String[] {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
         int n = (int) (Math.random() * 1_000_000);
         int result = n * 3;
         result = result + 0b10101;
         result = result + 0xFF;
         result = result * 6;
-        int  sum  = 0; 
+        int  sum  = 0;
         while( result > 0 || sum > 9){
             if(result != 0){
                 sum = sum + result % 10;
@@ -22,4 +22,3 @@ class HelloWorld {
 
     }
 }
-
